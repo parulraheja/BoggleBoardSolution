4eb0a5c26bb51303b283ee363e309f3452dd2c6a
parul <parulraheja296@gmail.com>
2019-12-18 03:07:15 +0530

    Input from user and Ui added

---
+++ Assets/Script/BoggleManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoggleManager : MonoBehaviour
+{
+    public InputField noOfRows;
+    public InputField noOfCols;
+    public Transform parentGrid;
+    public InputField prefabGrid;
+
+    public InputField noOfValidElments;
+    public Transform parentDict;
+    public InputField prefabDictInput;
+    public List<InputField> dictText;
+
+
+    public List <InputField> gridText;
+
+    public char[,] board;
+    public Dictionary<string, string> possibleWords = new Dictionary<string, string>();
+
+    public void OnSubmitBtn()
+    {
+         for (int i = 0; i < int.Parse(noOfRows.text); i++)
+        {
+            for (int j = 0; j < int.Parse(noOfCols.text); j++)
+
+            {
+              gridText.Add(Instantiate(prefabGrid, parentGrid));
+
+            }
+        }
+    }
+
+
+    public void AddTextValuesToBoard()
+    {
+        int count = 0;
+        board = new char[int.Parse(noOfRows.text), int.Parse(noOfCols.text)];
+
+        for (int i = 0; i < int.Parse(noOfRows.text); i++)
+        {
+            for (int j = 0; j < int.Parse(noOfCols.text); j++)
+
+            {    
+
+                board[i, j] = char.Parse(gridText[count].text);
+                count++;
+            }
+        }
+    }
+
+    public void IntantiateInputField()
+    {
+       for(int i=0;i< int.Parse(noOfValidElments.text);i++)
+        {
+            dictText.Add(Instantiate(prefabDictInput, parentDict));
+
+        }
+    }
+
+    public void addValuesToValidDictionary()
+    {
+        int index = 0;
+        for (int i = 0; i < int.Parse(noOfValidElments.text); i++)
+        {
+            possibleWords.Add(dictText[index].text, dictText[index].text);
+
+                index++;
+        }
+    }
+
+    public void TraverseElementsInBoggleBoard()
+    {
+
+    }
+
+
+}
